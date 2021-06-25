8920b7e627d5dc01cecc97c315872f2c2d89c83b
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 15:53:48 -0300

    feat: add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import cors from "cors"
 import { router } from "./routes";
 import { ApplicationError } from "@utils/errors";
 
 import "./database";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
