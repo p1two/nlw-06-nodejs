83260460ddfb749f9fdfa7865cf79ded48f5d4b7
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 13:24:30 -0300

    chore: add create user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,43 @@
+import { User } from "@entities/User";
+import { UsersRepositories } from "@repositories/UsersRepositories";
+import { getCustomRepository } from "typeorm";
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({
+    name,
+    email,
+    admin = false,
+  }: IUserRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepositories);
+
+    if (!email) {
+      throw new Error("Incorrect email");
+    }
+
+    const userAlreadyExists = await userRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
