2cb905febbc2a9217ecfed521d9443b7c7854bcc
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 14:07:40 -0300

    feat: add create tag endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
+import { CreateTagController } from "@controllers/CreateTagController";
 import { CreateUserController } from "@controllers/CreateUserController";
 import { Router } from "express";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
+router.post("/tags", createTagController.handle);
 
 export { router };
---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,36 @@
+import { Tag } from "@entities/Tag";
+import { TagsRepositories } from "@repositories/TagsRepositories";
+import { BadRequest } from "@utils/errors";
+import { getCustomRepository } from "typeorm";
+
+interface ITagRequest {
+  name: string;
+}
+
+class CreateTagService {
+  async execute({ name }: ITagRequest): Promise<Tag> {
+    const tagsRepositories = getCustomRepository(TagsRepositories);
+
+    if (!name) {
+      throw new BadRequest("Incorrect name");
+    }
+
+    const tagAlreadyExists = await tagsRepositories.findOne({
+      name,
+    });
+
+    if (tagAlreadyExists) {
+      throw new BadRequest("Tag already exists");
+    }
+
+    const tag = tagsRepositories.create({
+      name,
+    });
+
+    await tagsRepositories.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
