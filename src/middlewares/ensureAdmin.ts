c2317911cfe3c85394e46350a935cbb0db393350
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 15:23:53 -0300

    fix: get user id on request to ensureAdmin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,12 +1,18 @@
+import { UsersRepositories } from "@repositories/UsersRepositories";
 import { Unauthorized } from "@utils/errors";
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
 
 export async function ensureAdmin(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  const admin = true;
+  const { user_id } = request;
+
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
 
   if (admin) {
     return next();
