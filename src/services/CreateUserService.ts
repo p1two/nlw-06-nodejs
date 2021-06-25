4d9bc455beb81a886f03c9da8da1f92c56a2564a
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 14:42:51 -0300

    chore: add password to user

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -6,6 +6,7 @@ import { getCustomRepository } from "typeorm";
 interface IUserRequest {
   name: string;
   email: string;
+  password: string;
   admin?: boolean;
 }
 
@@ -13,12 +14,17 @@ class CreateUserService {
   async execute({
     name,
     email,
+    password,
     admin = false,
   }: IUserRequest): Promise<User> {
     const userRepository = getCustomRepository(UsersRepositories);
 
     if (!email) {
-      throw new BadRequest("Incorrect email");
+      throw new BadRequest("Email cannot be empty");
+    }
+
+    if (!password) {
+      throw new BadRequest("Password cannot be empty");
     }
 
     const userAlreadyExists = await userRepository.findOne({
@@ -33,6 +39,7 @@ class CreateUserService {
       name,
       email,
       admin,
+      password,
     });
 
     await userRepository.save(user);
