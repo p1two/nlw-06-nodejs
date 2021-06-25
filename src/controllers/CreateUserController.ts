fdb4af701d9d33b7b7eba9ebcd878e0f67e175d9
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 13:29:29 -0300

    chore: add create user controller

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { CreateUserService } from "@services/CreateUserService";
+import { Request, Response } from "express";
+
+class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const { name, email, admin } = request.body;
+
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      admin,
+    });
+
+    return response.json(user);
+  }
+}
+
+export { CreateUserController };
---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { CreateUserController } from "@controllers/CreateUserController";
+import { Router } from "express";
+
+const router = Router();
+
+const createUserController = new CreateUserController();
+
+router.post("/users", createUserController.handle);
+
+export { router };
