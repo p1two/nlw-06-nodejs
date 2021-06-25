cc9592d462c4e73b62b95a7a1b4227ce66e5fe63
Mozart Pistori Tomazetti <tomazetti@celepar.pr.gov.br>
2021-06-25 15:18:14 -0300

    feat: add ensureAuthenticated middleware

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateComplimentController } from "@controllers/CreateComplimentControl
 import { CreateTagController } from "@controllers/CreateTagController";
 import { CreateUserController } from "@controllers/CreateUserController";
 import { ensureAdmin } from "@middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@middlewares/ensureAuthenticated";
 import { Router } from "express";
 
 const router = Router();
@@ -14,7 +15,16 @@ const createComplimentController = new CreateComplimentController();
 
 router.post("/login", authenticateUserController.handle);
 router.post("/users", createUserController.handle);
-router.post("/tags", ensureAdmin, createTagController.handle);
-router.post("/compliments", createComplimentController.handle);
+router.post(
+  "/tags",
+  ensureAuthenticated,
+  ensureAdmin,
+  createTagController.handle
+);
+router.post(
+  "/compliments",
+  ensureAuthenticated,
+  createComplimentController.handle
+);
 
 export { router };
